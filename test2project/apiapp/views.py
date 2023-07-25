eda915a90e9b4c32aa050d04ec304137c5715c49
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-07-25 16:01:06 +0600

    update

--- test2project/apiapp/views.py
+++ test2project/apiapp/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework import status
+
+
+
 
 @api_view(['GET','POST','PUT','PATCH','DELETE'])
 def  student_api(request, pk=None):
     if request.method == 'GET':
-        id = pk
-        if id is None:
-            stu = Student.objects.get(id=id)
+        
+        if pk is not None:
+            stu = Student.objects.get(id=pk)
             serializer= StudentSerializer(stu)
             return Response(serializer.data)
         else:
@@ -21,35 +25,36 @@ def  student_api(request, pk=None):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response({'message': 'Post created successfull.'}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=400)
-        
+    
+    
     if request.method == 'PUT':
-        serializer = StudentSerializer(data=request.data, instance=pk)
+        student = get_object_or_404(Student, pk=pk)
+        serializer = StudentSerializer(data=request.data, instance=student)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data , status=201)
+            return Response({'message': 'PUT Update successfull.'}, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=400)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
         
     if request.method == 'PATCH':
-        serializer = StudentSerializer(data=request.data, instance=pk)
+        student = get_object_or_404(Student, pk=pk)
+        serializer = StudentSerializer(data=request.data, instance=student)
+        
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response({'message': 'PATCH Update successful.'}, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=400)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
         
     if request.method == 'DELETE':
-        stu = Student.objects.get(id=pk)
-        stu.delete()
-        return Response(status=204) 
+        try:
+            stu = Student.objects.get(id=pk)
+            stu.delete()
+            return Response({'message': 'DELETE operation successful.'}, status=status.HTTP_204_NO_CONTENT)
+        except Student.DoesNotExist:
+            return Response({'message': 'Student not found.'}, status=status.HTTP_404_NOT_FOUND) 
         
-            
-
-
-            
-    
-
-
--- test2project/test2project/urls.py
+++ test2project/test2project/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from apiapp import views
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ggg', views.student_api),
+    path('',include('apiapp.urls')),
 ]
